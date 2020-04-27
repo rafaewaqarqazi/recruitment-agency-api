fb3f6188acacf1571dde7a13a4a33153b3e1f96f
rafaewaqarqazi <rafaewaqar@gmail.com>
2020-04-27 23:25:03 +0500

    Job Delete Completed!

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -14,6 +14,37 @@ exports.newJob = async (req, res) => {
     });
   }
 }
+exports.editJob = async (req, res) => {
+  const body = req.body
+  const editData = {...body}
+  delete editData._id
+  const job = await Jobs.findByIdAndUpdate(body._id, {
+    ...editData
+  }, {new: true})
+  if (job) {
+    await res.json({
+      success: true,
+      job
+    });
+  }
+}
+exports.deleteJob = async (req, res) => {
+  const body = req.body
+  try {
+    const response = await Jobs.findByIdAndDelete(body.id)
+    console.log('response', response)
+    if (response) {
+      await res.json({
+        success: true,
+        message: 'Job Post Deleted Successfully!'
+      });
+    } else {
+      await res.json({success: false, message: 'could not delete job post'})
+    }
+  } catch (e) {
+    await res.json({success: false, message: 'could not delete job post'})
+  }
+}
 exports.allJobs = async (req, res) => {
   const jobs = await Jobs.find();
   await res.json({
--- routes/jobs.js
+++ routes/jobs.js
@@ -3,10 +3,14 @@ const {isAdmin} = require("../controllers/auth");
 const router = express.Router();
 const {
   newJob,
-  allJobs
+  editJob,
+  allJobs,
+  deleteJob
 } = require('../controllers/jobs');
 const {requireSignin} = require('../controllers/auth');
 
 router.post('/new', requireSignin, isAdmin, newJob);
+router.put('/edit', requireSignin, isAdmin, editJob);
+router.put('/delete', requireSignin, isAdmin, deleteJob);
 router.get('/all', allJobs);
 module.exports = router;
